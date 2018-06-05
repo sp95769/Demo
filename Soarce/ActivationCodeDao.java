118c00f17f36de85b1c76f91c93a11678bdbb60c
JDNET\sp95769 <pingalesunil@johndeere.com>
2018-06-05 13:27:09 +0530

    My first commit

---
+++ Soarce/ActivationCodeDao.java
@@ -0,0 +1,9 @@
+package com.deere.u90787.saweb.dao;
+
+import com.deere.u90787.saweb.domain.jpa.ActivationCode;
+
+public interface ActivationCodeDao {
+	
+	public Boolean insertActivationCodeForDealerAccount(ActivationCode activationCode)throws Exception;
+
+}
